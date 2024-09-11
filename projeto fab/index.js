a39f8d0ebd24dce3f93af5991f1d78ddbf8cdc7e
WILLIANS KEIICHI SILVEIRA SATO <WILLIANS5489@IOT>
2024-09-11 11:54:56 -0300

    atualizar

--- projeto fab/index.js
+++ projeto fab/index.js
@@ -1,24 +1,50 @@
 const { log } = require('console');
 const express = require('express')
-const { data } = require('./models/DUMMY')
+const { client } = require('./models/DUMMY')
 
 const app = express();
 
 app.use(express.json());
 
+// app.get('/', (req, res) => {
+//     res.send(data);
+// });
 app.get('/', (req, res) => {
-    res.send(data);
+    res.send(client);
 });
 
 
-app.get('/:id', (req, res) => {
+app.get('/cliente/:id', (req, res) => {
     const { id } = req.params;
-    const datas = data.find((value) => value.id === Number(id));
-    if (!datas) return res.send('client not found');
-    res.send(datas)
-})
+    const clientes = client.find((value) => value.id === Number(id));
+    if (!clientes) return res.send('client not found');
+    res.send(clientes)
+});
 
-app.get('')
+app.post('/cliente', (req, res) => {
+    const { name } = req.body;
+    const id = client.length + 1;
+    client.push({ id, name });
+    res.send('Client added')
+});
+
+app.put('/cliente/:id',(req,res) =>{
+    const {id} = req.params;
+    const {name} = req.body;
+    const clientes = client.find((value) => value.id === Number(id));
+    if (!clientes) return res.send('client not found');
+    clientes.name=name;
+    res.send("cliente updated")
+});
+
+app.delete('/cliente/:id',(req,res)=>{
+    const {id} = req.params;
+    const index = client.findIndex((value)=>value.id===Number(id));
+    if (index === -1) return res.send("cliente not found");
+    client.splice(index,1);
+    res.send("client apagado");
+});
+
 app.listen(8080, () => {
     console.log('ta rodando o serve');
 
